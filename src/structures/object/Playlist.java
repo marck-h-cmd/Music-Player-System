7bdcf8d486da6376bbd928413d719f8e35eb53d4
Pandaman <147335739+Pandaman08@users.noreply.github.com>
2024-11-27 16:51:31 -0500

    feat: feat/playlist

--- src/structures/object/Playlist.java
+++ src/structures/object/Playlist.java
@@ -4,10 +4,59 @@
  */
 package structures.object;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author marck
  */
 public class Playlist {
+    private String name;
+    private ArrayList<Song> songs;
+    private double durationTotal;
+    private int numSongs; 
+
+    public Playlist() {
+        this("NN",new ArrayList<>(),0.0,0);
+    }
     
+    
+    public Playlist(String name, ArrayList<Song> songs, double durationTotal, int numSongs) {
+        this.name = name;
+        this.songs = songs;
+        this.durationTotal = durationTotal;
+        this.numSongs = numSongs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(ArrayList<Song> songs) {
+        this.songs = songs;
+    }
+
+    public double getDurationTotal() {
+        return durationTotal;
+    }
+
+    public void setDurationTotal(double durationTotal) {
+        this.durationTotal = durationTotal;
+    }
+
+    public int getNumSongs() {
+        return numSongs;
+    }
+
+    public void setNumSongs(int numSongs) {
+        this.numSongs = numSongs;
+    }
 }
